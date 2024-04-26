1b8cfcf413e298d66bf0e439bc4e90ac58ae7795
Guilherme <guilherme.1009@hotmail.com>
2024-04-26 01:32:33 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -11,31 +11,50 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> categories = repository.findAll();
 		return categories.stream().map(item -> new CategoryDTO(item)).toList();
 	}
-	
+
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
-		Category category = repository.findById(id).orElseThrow(
-				() -> new ResourceNotFoundException("Recurso não encontrado"));
-		
+		Category category = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+
 		return new CategoryDTO(category);
 	}
-	
+
 	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category(dto);
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
-	
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			entity = dtoToEntity(entity, dto);
+			repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id inválido, Recurso não encontrado");
+		}
+	}
+
+	public Category dtoToEntity(Category entity, CategoryDTO dto) {
+		entity.setName(dto.getName());
+		return entity;
+	}
+
 }
